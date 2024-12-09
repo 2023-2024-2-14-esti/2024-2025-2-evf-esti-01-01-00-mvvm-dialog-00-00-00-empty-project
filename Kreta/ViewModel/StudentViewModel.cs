cceb57ffe2314df4ff8752e149e8e2c1c9854871
Grezsa Zoltán <bsze3grezol@vasvari.org>
2024-12-09 19:44:57 +0100

    StudentViewModel létrehozása és bindingolása a MainWindow-val

---
+++ Kreta/ViewModel/StudentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Kreta.Models;
+
+namespace Kreta.ViewModel
+{
+    public partial class StudentViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Student _student = new Student();
+    }
+}
