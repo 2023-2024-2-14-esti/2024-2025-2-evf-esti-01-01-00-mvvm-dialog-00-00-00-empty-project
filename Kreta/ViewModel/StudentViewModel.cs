eced7223ad361cd8bf83c4b2fdf26f67db40403c
Grezsa Zoltán <bsze3grezol@vasvari.org>
2024-12-09 19:56:44 +0100

    StudentViewModel kiegészítése

--- Kreta/ViewModel/StudentViewModel.cs
+++ Kreta/ViewModel/StudentViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Kreta.Models;
+using System.Collections.ObjectModel;
 
 namespace Kreta.ViewModel
 {
@@ -7,5 +9,35 @@ namespace Kreta.ViewModel
     {
         [ObservableProperty]
         private Student _student = new Student();
+
+        [ObservableProperty]
+        private List<String> _educationLevels = new List<String>()
+        { "érettségi", "szakmai érettségi", "szakmai vizsga" };
+
+        [ObservableProperty]
+        private ObservableCollection<Student> _students = new ObservableCollection<Student>();
+
+        public StudentViewModel()
+        {
+            _student.BirthsDay = DateTime.Now;
+        }
+
+        [RelayCommand]
+        public void DoAddNewStudent(Student newStudent)
+        {
+            Students.Add(newStudent);
+        }
+
+        [RelayCommand]
+        public void DoRemoveStudent(Student studentToDelete)
+        {
+            Students.Remove(studentToDelete);
+        }
+
+        [RelayCommand]
+        public void DoNewStudent()
+        {
+            Student = new Student();
+        }
     }
 }
